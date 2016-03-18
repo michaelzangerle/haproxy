277899d3ad9e6aef355cc580b9ad80bb53137971
Michael Zangerle <michael.zangerle@gmail.com>
2016-03-18 11:48:44 +0100

    added session stickiness exmample code

--- nginx/files/usr/share/nginx/html/index.php
+++ nginx/files/usr/share/nginx/html/index.php
@@ -7,3 +7,17 @@ echo "\nClient IP: ".$_SERVER['REMOTE_ADDR'];
 if(array_key_exists("HTTP_X_FORWARDED_FOR", $_SERVER)) {
     echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
 }
+
+session_start();
+
+if(!isset($_SESSION['visit']))
+{
+        echo "\n\nThis is the first time you're visiting this server!\n";
+        $_SESSION['visit'] = 1;
+}
+else {
+        echo "\n\nYour number of visits: ".$_SESSION['visit']."\n";
+        print_r($_COOKIE);
+}
+
+$_SESSION['visit']++;
