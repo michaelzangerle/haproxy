59e1a2c41aff4dcd44d19d42e75cf3431c164974
Michael Zangerle <michael.zangerle@gmail.com>
2016-06-13 09:04:58 +0200

    added some documenation links

--- haproxy/files/usr/local/bin/restart_haproxy.sh
+++ haproxy/files/usr/local/bin/restart_haproxy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# http://serverfault.com/questions/580595/haproxy-graceful-reload-with-zero-packet-loss
+# http://engineeringblog.yelp.com/2015/04/true-zero-downtime-haproxy-reloads.html
+# https://github.com/jwilder/docker-discover/blob/master/reload-haproxy.sh
+# https://medium.com/@Drew_Stokes/actual-zero-downtime-with-haproxy-18318578fde6#.7edt1jc1n
 
 # hold/pause new requests
 iptables -I INPUT -p tcp --dport 8080 --syn -j DROP
-sleep 1
+sleep 0.5
 
 if [ -f "/var/run/haproxy.pid" ] ; then
     # gracefully restart haproxy
