574c4b33ff0d3a9c971aacb47562bc6f21995dba
Michael Zangerle <michael.zangerle@gmail.com>
2016-06-13 09:06:28 +0200

    reverted changes for demo

--- nginx/files/usr/share/nginx/html/index.php
+++ nginx/files/usr/share/nginx/html/index.php
@@ -8,18 +8,18 @@ if(array_key_exists("HTTP_X_FORWARDED_FOR", $_SERVER)) {
     echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
 }
 
-// sleep(3);
+sleep(3);
 
-// session_start();
+session_start();
 
-// if(!isset($_SESSION['visit']))
-// {
-//         echo "\n\nThis is the first time you're visiting this server!\n";
-//         $_SESSION['visit'] = 1;
-// }
-// else {
-//         echo "\n\nYour number of visits: ".$_SESSION['visit']."\n";
-//         print_r($_COOKIE);
-// }
+if(!isset($_SESSION['visit']))
+{
+        echo "\n\nThis is the first time you're visiting this server!\n";
+        $_SESSION['visit'] = 1;
+}
+else {
+        echo "\n\nYour number of visits: ".$_SESSION['visit']."\n";
+        print_r($_COOKIE);
+}
 
-// $_SESSION['visit']++;
+$_SESSION['visit']++;
